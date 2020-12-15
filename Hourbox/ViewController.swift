391d400127f22079cc248217fc4dc33d5fa4259c
felipecondor <fcorrea@condorlabs.io>
2020-12-14 20:19:53 -0600

    Initial Commit

---
+++ Hourbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hourbox
+//
+//  Created by Felipe Correa on 14/12/20.
+//  Copyright © 2020 Felipe & Co. Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
